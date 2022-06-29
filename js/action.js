746788bbea8617a474de4dcaaee371fa5adea140
kurokawalei <kurokawa2011@gmail.com>
2022-06-29 15:26:22 +0800

    整包

---
+++ js/action.js
@@ -0,0 +1,44 @@
+//共用載入
+
+$('#header').load('../header.html');
+$('footer').load('../footer.html');
+
+
+
+$('#sliderBox').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  items: 1,
+  nav: true,
+  navText: ['<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>']
+
+});
+
+$('.allModelList').owlCarousel({
+  loop: true,
+  margin: 15,
+  dots: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+      nav: false,
+      dots:true
+    },
+    600: {
+      items: 3,
+      nav: false
+    },
+    1000: {
+      items: 4,
+      nav: false,
+      loop: false,
+      mouseDrag:false
+    }
+  }
+});
+
+
+
+
